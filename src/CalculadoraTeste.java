7b1c7d0fecc26ca9b406c7af40158f67c8610df3
System Administrator <root@Guilhermes-MacBook-Pro.local>
2015-11-04 01:29:08 -0200

    Adição da anotação @before

--- src/CalculadoraTeste.java
+++ src/CalculadoraTeste.java
@@ -1,24 +1,26 @@
- 
-    import org.junit.Test;
-    import static org.junit.Assert.assertEquals;
 
-    public class CalculadoraTeste {
-    
-        @Test
-        public void deveriaSomarDoisValoresPassados() throws Exception {
-            int valorA = 1;
-            int valorB = 3;
-            double soma = 0;
-            Calculadora calculadora = new Calculadora();
-            soma = calculadora.soma(valorA, valorB);
-            assertEquals(4, soma);
-        }
-        
-    /*    @Test
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculadoraTeste {
+	//Calculadora calculadora; -> Ignore
+
+	@Test
+	public void deveriaSomarDoisValoresPassados() throws Exception {
+		int valorA = 1;
+		int valorB = 3;
+		double soma = 0;
+		Calculadora calculadora = new Calculadora();
+		soma = calculadora.soma(valorA, valorB);
+		assertEquals(4, soma, 0.00001);
+	}
+
+	/*  @Test
         public void nomeDoMetodoParaRealizarAOperacaoMultiplicacao() throws Exception{
         	//Implementação da função multiplicação
         }
-        
+
         @Test void nomeDoMetodoParaRealizarAOperacaoSubtracao() throws Exception{
         	//Implementação da função subtração
         }
@@ -29,6 +31,14 @@
        }
        @Test
        public void nomeDoMetodoParaCasoAcontecerUmaDivisaoPorZero() throws Exception{
-    	   	
-       }*/
+
+       }
+	 */
+
+
+	/*	Exemplo do uso da anotação @before
+	 *  @Before
+             public void inicializa(){
+             	this.calculadora = new Calculadora();
+             }*/
 }
