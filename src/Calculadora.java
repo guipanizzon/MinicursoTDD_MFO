f105708ab7f1fa3929a620eeea9c8ff4ba0221f8
System Administrator <root@Guilhermes-MacBook-Pro.local>
2015-11-04 01:09:00 -0200

    Código fonte do minicurso

---
+++ src/Calculadora.java
@@ -0,0 +1,11 @@
+
+public class Calculadora {
+    public double soma(int valorA, int valorB) {
+        return valorA + valorB;
+    }
+    
+    //criação do método multiplicação
+    //criação do método subtração
+    //criação do método divisão
+
+}
